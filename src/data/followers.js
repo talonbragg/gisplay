032231d7696e5b6897b92636ae895353b84c9c94
Talon Bragg <pvpgamecrash@gmail.com>
2017-03-04 17:55:29 +0000

    Create followers.js

---
+++ src/data/followers.js
@@ -0,0 +1,12 @@
+function followers(un) {
+  fetch('https://api.github.com/users/' + un)
+    .then(res => {
+      return res.json
+    })
+    .then(data => {
+      for (var i = 0; i < data.length; i++) {
+        if (data[i].site_admin !== true) {
+          console.log(data[i].followers)
+        }
+    });
+}
